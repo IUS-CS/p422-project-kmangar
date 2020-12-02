1757059b99f24a98154a974fa7aa5f5cfeb28721
kmangar <kham.mangar@outlook.com>
2020-12-02 00:10:28 -0500

    added header and table to routes

--- currency-exchange-app/src/app/app-routing.module.ts
+++ currency-exchange-app/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConverterComponent } from './components/converter/converter.component';
 import { PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+// @ts-ignore
+import { TableComponent} from './components/table/table-Component.component';
 
 const routes: Routes = [
   {path: 'converter', component: ConverterComponent},
   {path: '', redirectTo: '/converter', pathMatch: 'full'},
-  {path: 'page-not-found', component: PageNotFoundComponent},
-  {path: '***', component: PageNotFoundComponent}
+  {path: 'table', component: TableComponent},
+  {path: 'history', component: TableComponent},
+  {path: '***', component: PageNotFoundComponent},
+
 ];
 
 @NgModule({
--- currency-exchange-app/src/app/components/header/header.component.ts
+++ currency-exchange-app/src/app/components/header/header.component.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  navbarOpen = false;
 
+  // tslint:disable-next-line:typedef
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
   ngOnInit(): void {
   }
+
 
 }
