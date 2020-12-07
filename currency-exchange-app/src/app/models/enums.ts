095c393f4a6b2b3837960af93b09d8781a3de9bc
kmangar <kham.mangar@outlook.com>
2020-12-07 03:49:19 -0500

    definition

---
+++ currency-exchange-app/src/app/models/enums.ts
@@ -0,0 +1,15 @@
+// for the form
+export enum FormNames {
+  FromControl = 'fromControl',
+  ToControl = 'toControl',
+  AmountControl = 'amountControl',
+}
+
+export enum Currency {
+  USD = 'USD',
+}
+// for stats
+export interface Statistics {
+  name: string;
+  summary: number;
+}
